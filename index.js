d730ad683edb726b0455181eabdfe7972c0a78f5
Mogalapalli-Sriram <srirammogalapalli238@gmail.com>
2023-11-03 18:44:53 +0530

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const sum = (a,b) => {
     return a+b;
 }
-console.log(`The Sum Is: ${sum}`);
+
+const difference = (a,b) => {
+    return a-b
+}
+
+console.log(`The Sum Is: ${sum(20,30)}`);
+console.log(`The Difference Is: ${difference(30,20)}`);
